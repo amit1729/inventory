9b12e0166730b5439d8a6e4486e37bac3b652363
Amit <amityadav8107474@gmail.com>
2022-01-18 12:48:07 +0530

    first working model

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchBar from "./SearchBar";
+import { useState, useEffect } from "react";
+import AddItem from "./AddItems";
+import ItemsDisplay from "./ItemsDisplay";
 
 function App() {
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((response) => response.json())
+      .then((data) => {
+        setData({ items: data });
+      });
+  }, []);
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  };
+
+  const deleteItem = (item) => {
+    const items = data["items"];
+    const requestOptions = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:3000/items/${item.id}`, requestOptions).then(
+      (response) => {
+        if (response.ok) {
+          const idx = items.indexOf(item);
+          items.splice(idx, 1);
+          setData({ items: items });
+        }
+      }
+    );
+  };
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    };
+
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        items.push(data);
+        setData({ items: items });
+      });
+  };
+
+  const filterData = (data) => {
+    const filteredData = [];
+    if (!filters.name) {
+      return data;
+    }
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+      filteredData.push(item);
+    }
+    return filteredData;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-3">
+        <ItemsDisplay
+          deleteItem={deleteItem}
+          items={filterData(data["items"])}
+        />
+      </div>
+      <div className="row mt-3">
+        <SearchBar updateSearchParams={updateFilters} />
+      </div>
+
+      <div className="row mt-3">
+        <AddItem AddItem={addItemToData} />
+      </div>
     </div>
   );
 }
+
+/*function ButtonState() {
+  const [title, setTitle] = useState("");
+  const [count, setCount] = useState(0);
+
+  const updateTitleClicked = () => {
+    setTitle("Whaat a title is here");
+  }
+
+  const updateCounterClicked = () => {
+    setCount(count+1);
+  }
+
+  return (
+    <div>
+      <Data title ={title} count = {count}/>
+      <button onClick = {updateTitleClicked}>Update Title</button>
+      <button onClick = {updateCounterClicked}>Update Counter</button>
+    </div>
+
+  );
+}
+
+function Data(props) {
+  return(<div>
+    <p>Title: {props.title}</p>
+    <p>Count: {props.count}</p>
+  </div>);
+}
+
+
+
+function AddItem(props) {
+  return (
+    <form>
+      <label for = "text-form">Type something</label>
+      <input type="text" value = {props.text} id = "text-form" />
+      <p>{props.number}</p>
+    </form>
+
+  );
+}
+
+AddItem.defaultProps = {
+  number: 1,
+  text: "default",
+}
+
+AddItem.prototype ={
+  number: PropTypes.number,
+}*/
 
 export default App;
